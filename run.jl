bf08c4184178b66ccfa3d8cbd7084e76aca470d7
leonardgoeke <lgo@wip.tu-berlin.de>
2022-10-05 17:32:59 +0200

    fixed bug in script

--- run.jl
+++ run.jl
@@ -32,7 +32,7 @@ heu_m, heuSca_obj = @suppress heuristicSolve(optMod_dic[:heuSca],1.0,t_int,Gurob
 fix_dic, lim_dic, cntHeu_arr = evaluateHeu(heu_m,heuSca_obj,heuCom_obj,(thrsAbs = 0.001, thrsRel = 0.05)) # get fixed and limited variables
 feasFix_dic = getFeasResult(optMod_dic[:top],fix_dic,lim_dic,t_int,0.001,Gurobi.Optimizer) # ensure feasiblity with fixed variables
 # ! write fixed variable values to files
-writeFixToFiles(fix_dic,feasFix_dic,temp_dir,heu_m; skipMustSt = true)
+writeFixToFiles(feasFix_dic,feasFix_dic,temp_dir,heu_m; skipMustSt = true)
 
 if isfile(temp_dir * "/par_FixTech_bevFrtRoadLight_expConv.csv") rm(temp_dir * "/par_FixTech_bevFrtRoadLight_expConv.csv") end
 if isfile(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") rm(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") end
