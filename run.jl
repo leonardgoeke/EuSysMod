511c79e6125629cc0174d1edc060d98f979a54f2
leonardgoeke <lgo@wip.tu-berlin.de>
2022-10-22 13:06:54 +0200

    testing greater resolutions than 1752/8760

--- run.jl
+++ run.jl
@@ -13,32 +13,35 @@ temp_dir = "tempFix_" * obj_str # directory for temporary folder
 if isdir(temp_dir) rm(temp_dir, recursive = true) end
 mkdir(temp_dir)
 
-inputMod_arr = ["_basis","nuCost/" * nuC,"nuYear/" * nuY,"timeSeries/" * h * "hours_2008_only2040",temp_dir]
-inputHeu_arr = ["_basis","nuCost/" * nuC,"nuYear/" * nuY,"timeSeries/" * h_heu * "hours_2008_only2040"]
-resultDir_str = "results"
+if h_heu != "8760"
 
-#region # * perform heuristic solve
+    inputMod_arr = ["_basis","nuCost/" * nuC,"nuYear/" * nuY,"timeSeries/" * h * "hours_2008_only2040",temp_dir]
+    inputHeu_arr = ["_basis","nuCost/" * nuC,"nuYear/" * nuY,"timeSeries/" * h_heu * "hours_2008_only2040"]
+    resultDir_str = "results"
 
-coefRngHeuSca_tup = (mat = (1e-2,1e4), rhs = (1e0,1e5))
-scaFacHeuSca_tup = (capa = 1e0, capaStSize = 1e2, insCapa = 1e1, dispConv = 1e1, dispSt = 1e3, dispExc = 1e3, dispTrd = 1e3, costDisp = 1e1, costCapa = 1e2, obj = 1e0)
+    #region # * perform heuristic solve
 
-optMod_dic = Dict{Symbol,NamedTuple}()
-optMod_dic[:heuSca] =  (inputDir = inputHeu_arr, resultDir = resultDir_str, suffix = obj_str, supTsLvl = 2, shortExp = 5, coefRng = coefRngHeuSca_tup, scaFac = scaFacHeuSca_tup)
-optMod_dic[:top] 	=  (inputDir = inputMod_arr, resultDir = resultDir_str, suffix = obj_str, supTsLvl = 2, shortExp = 5, coefRng = coefRngHeuSca_tup, scaFac = scaFacHeuSca_tup)
+    coefRngHeuSca_tup = (mat = (1e-2,1e4), rhs = (1e0,1e5))
+    scaFacHeuSca_tup = (capa = 1e0, capaStSize = 1e2, insCapa = 1e1, dispConv = 1e1, dispSt = 1e3, dispExc = 1e3, dispTrd = 1e3, costDisp = 1e1, costCapa = 1e2, obj = 1e0)
 
-heu_m, heuSca_obj = @suppress heuristicSolve(optMod_dic[:heuSca],1.0,t_int,Gurobi.Optimizer);
-~, heuCom_obj = @suppress heuristicSolve(optMod_dic[:heuSca],8760/parse(Int,h_heu),t_int,Gurobi.Optimizer)
-# ! write fixes to files and limits to dictionary
-fix_dic, lim_dic, cntHeu_arr = evaluateHeu(heu_m,heuSca_obj,heuCom_obj,(thrsAbs = 0.001, thrsRel = 0.05),true) # get fixed and limited variables
-feasFix_dic = getFeasResult(optMod_dic[:top],fix_dic,lim_dic,t_int,0.001,Gurobi.Optimizer) # ensure feasiblity with fixed variables
-# ! write fixed variable values to files
-writeFixToFiles(fix_dic,feasFix_dic,temp_dir,heu_m; skipMustSt = true)
+    optMod_dic = Dict{Symbol,NamedTuple}()
+    optMod_dic[:heuSca] =  (inputDir = inputHeu_arr, resultDir = resultDir_str, suffix = obj_str, supTsLvl = 2, shortExp = 5, coefRng = coefRngHeuSca_tup, scaFac = scaFacHeuSca_tup)
+    optMod_dic[:top] 	=  (inputDir = inputMod_arr, resultDir = resultDir_str, suffix = obj_str, supTsLvl = 2, shortExp = 5, coefRng = coefRngHeuSca_tup, scaFac = scaFacHeuSca_tup)
 
-if isfile(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") rm(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") end
-if isfile(temp_dir * "/par_FixTech_nuclearPower_capaConv.csv") rm(temp_dir * "/par_FixTech_nuclearPower_capaConv.csv") end
+    heu_m, heuSca_obj = @suppress heuristicSolve(optMod_dic[:heuSca],1.0,t_int,Gurobi.Optimizer);
+    ~, heuCom_obj = @suppress heuristicSolve(optMod_dic[:heuSca],8760/parse(Int,h_heu),t_int,Gurobi.Optimizer)
+    # ! write fixes to files and limits to dictionary
+    fix_dic, lim_dic, cntHeu_arr = evaluateHeu(heu_m,heuSca_obj,heuCom_obj,(thrsAbs = 0.001, thrsRel = 0.05),true) # get fixed and limited variables
+    feasFix_dic = getFeasResult(optMod_dic[:top],fix_dic,lim_dic,t_int,0.001,Gurobi.Optimizer) # ensure feasiblity with fixed variables
+    # ! write fixed variable values to files
+    writeFixToFiles(fix_dic,feasFix_dic,temp_dir,heu_m; skipMustSt = true)
 
-# add capa and gas 2x
-heu_m = nothing
+    if isfile(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") rm(temp_dir * "/par_FixTech_nuclearPower_expConv.csv") end
+    if isfile(temp_dir * "/par_FixTech_nuclearPower_capaConv.csv") rm(temp_dir * "/par_FixTech_nuclearPower_capaConv.csv") end
+
+    heu_m = nothing
+
+end
 
 #endregion
 
