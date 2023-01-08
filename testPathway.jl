e4aa10795fcd882897f9865782f6cba451254a18
leonardgoeke <lgo@wip.tu-berlin.de>
2023-01-09 00:07:00 +0100

    update model files

--- testPathway.jl
+++ testPathway.jl
@@ -1,40 +1,37 @@
-
+b = "C:/Users/pacop/.julia/dev/AnyMOD.jl/"
+	
 using Base.Threads, CSV, Dates, LinearAlgebra, Requires, YAML
-using MathOptInterface, Reexport, Statistics, PyCall, SparseArrays
-using DataFrames, JuMP, Suppressor
+using MathOptInterface, Reexport, Statistics, SparseArrays
+using DataFrames, JuMP, Suppressor, Plotly
+using DelimitedFiles
 
-pyimport_conda("networkx","networkx")
-pyimport_conda("matplotlib.pyplot","matplotlib")
-pyimport_conda("plotly","plotly")
+include(b* "src/objects.jl")
+include(b* "src/tools.jl")
+include(b* "src/modelCreation.jl")
+include(b* "src/decomposition.jl")
 
-include("src/objects.jl")
-include("src/tools.jl")
-include("src/modelCreation.jl")
-include("src/decomposition.jl")
+include(b* "src/optModel/technology.jl")
+include(b* "src/optModel/exchange.jl")
+include(b* "src/optModel/system.jl")
+include(b* "src/optModel/cost.jl")
+include(b* "src/optModel/other.jl")
+include(b* "src/optModel/objective.jl")
 
-include("src/optModel/technology.jl")
-include("src/optModel/exchange.jl")
-include("src/optModel/system.jl")
-include("src/optModel/cost.jl")
-include("src/optModel/other.jl")
-include("src/optModel/objective.jl")
+include(b* "src/dataHandling/mapping.jl")
+include(b* "src/dataHandling/parameter.jl")
+include(b* "src/dataHandling/readIn.jl")
+include(b* "src/dataHandling/tree.jl")
+include(b* "src/dataHandling/util.jl")
 
-include("src/dataHandling/mapping.jl")
-include("src/dataHandling/parameter.jl")
-include("src/dataHandling/readIn.jl")
-include("src/dataHandling/tree.jl")
-include("src/dataHandling/util.jl")
-
-include("src/dataHandling/gurobiTools.jl")
-
+include(b* "src/dataHandling/gurobiTools.jl")
 
 using Gurobi
 
 h = "96"
 
-b = "C:/Users/pacop/Desktop/alternate versions/pathwayfull/" # add the model dir here
+b = "" # add the model dir here
 
-input_arr = [b * "_basis",b * "timeSeries/" * h * "hours_2008_only2020"]
+input_arr = [b * "_basis",b * "timeSeries/" * h * "hours_2008"]
 resultDir_str = b * "results"
 
 anyM = anyModel(input_arr, resultDir_str, objName = "TheModel", supTsLvl = 2, reportLvl = 2, shortExp = 5, emissionLoss = false)
@@ -46,28 +43,6 @@ set_optimizer_attribute(anyM.optModel, "Method", 2);
 set_optimizer_attribute(anyM.optModel, "Crossover", 0);
 optimize!(anyM.optModel)
 
-reportResults(:summary,anyM, addRep = (:capaConvOut,), addObjName = true)
-reportResults(:exchange,anyM, addObjName = true)
-reportResults(:cost,anyM, addObjName = true)
-
+reportResults(:summary,anyM, addObjName = true)
 
 printIIS(anyM)
-
-b = "C:/Users/pacop/Downloads/sdf/"
-
-input_arr = [b * "modelData"]
-resultDir_str = b * "results"
-
-anyM = anyModel(input_arr, resultDir_str, objName = "TheModel")
-
-
-include("src/optModel/tech.jl")
-include("src/optModel/exchange.jl")
-include("src/optModel/other.jl")
-include("src/optModel/objective.jl")
-
-include("src/dataHandling/mapping.jl")
-include("src/dataHandling/parameter.jl")
-include("src/dataHandling/readIn.jl")
-include("src/dataHandling/tree.jl")
-include("src/dataHandling/util.jl")
