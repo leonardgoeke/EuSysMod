45eb9f1b88895707899db37ec4fa3f0762640cff
leonardgoeke <lgo@wip.tu-berlin.de>
2023-01-28 17:38:38 +0100

    updated job scripts

--- run.jl
+++ run.jl
@@ -2,22 +2,22 @@ using AnyMOD, Gurobi, CSV, Statistics
 
 # ! string here define scenario, overwrite ARGS with respective values for hard-coding scenarios according to comments
 h = ARGS[1] # resolution of time-series for actual solve, can be 96, 1752, 4392, or 8760
-
-b = "" # add the model dir here
+t_int = parse(Int,ARGS[2]) # number of threads
 
 input_arr = ["_basis","timeSeries/" * h * "hours_2008"]
 resultDir_str = "results"
 
-anyM = anyModel(input_arr, resultDir_str, objName = "TheModel", supTsLvl = 2, reportLvl = 2, shortExp = 5, emissionLoss = false)
+anyM = anyModel(input_arr, resultDir_str, objName = "pathway_h" * h, supTsLvl = 2, reportLvl = 2, shortExp = 5, emissionLoss = false)
 createOptModel!(anyM)
 setObjective!(:cost,anyM)
 
 set_optimizer(anyM.optModel, Gurobi.Optimizer)
 set_optimizer_attribute(anyM.optModel, "Method", 2);
 set_optimizer_attribute(anyM.optModel, "Crossover", 0);
+set_optimizer_attribute(anyM.optModel, "Threads",t_int);
 optimize!(anyM.optModel)
 
-computeResults("aggBenchmark.yml",model = anyM, outputDir = "results/")
+computeResults("aggBenchmark.yml",model = anyM, outputDir = "results/", addName = "pathway_h" * h)
 
 reportResults(:summary,anyM)
 
--- testPathway.jl
+++ testPathway.jl
@@ -52,3 +52,27 @@ printIIS(anyM)
 
 printDuals(vcat(anyM.parts.bal.cns[:enBalH2],anyM.parts.bal.cns[:enBalGasFuel],anyM.parts.bal.cns[:enBalNaturalGas]),anyM)
 printObject(anyM.parts.tech[:directAirCapture].cns[:convBal],anyM)
+
+anyM.parts.tech[:avaAndNaviTech]
+
+inDir = input_arr
+outDir = resultDir_str
+
+objName = ""
+
+csvDelim = ","
+interCapa = :linear
+supTsLvl = 2
+shortExp = 5
+redStep = 1.0
+holdFixed = false
+emissionLoss = true
+forceScr = nothing
+reportLvl = 2
+errCheckLvl = 1
+errWrtLvl = 1
+coefRng = (mat = (1e-2,1e4), rhs = (1e-2,1e2))
+scaFac = (capa = 1e2,  capaStSize = 1e2, insCapa = 1e1,dispConv = 1e3, dispSt = 1e5, dispExc = 1e3, dispTrd = 1e3, costDisp = 1e1, costCapa = 1e2, obj = 1e0)
+bound = (capa = NaN, disp = NaN, obj = NaN)
+avaMin = 0.01
+checkRng = (print = false, all = true)
