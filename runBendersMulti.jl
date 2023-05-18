d7184b5a920d5fb8332e317d67b9f0b271ce53ae
leonardgoeke <lgoeke@ethz.ch>
2023-05-18 13:08:48 +0200

    updated script

--- runBendersMulti.jl
+++ runBendersMulti.jl
@@ -9,7 +9,7 @@ method = parse(Int,ARGS[1])
 if method == 0 
 	meth_tup = tuple()
 elseif method == 1
-	meth_tup = (:prx => (start = 1.0, low = 0.0, fac = 2.0),)
+	meth_tup = (:prx => (start = 1.0, low = 1e-4, fac = 2.0),)
 elseif method == 2
 	meth_tup = (:lvl => (la = 0.5,),)
 elseif method == 3
@@ -99,7 +99,7 @@ nb_workers = scr * 2
 @static if Sys.islinux()
 	# MatheClusterManagers is an altered version of https://github.com/JuliaParallel/ClusterManagers.jl by https://github.com/mariok90 to run on the cluster of TU Berlin
 	using MatheClusterManagers
-    qrsh(nb_workers, timelimit=345600, ram=ram, mp = t_int)
+	qsub(4, timelimit=345600, ram=ram, mp = t_int)
 	#addprocs_slurm(nb_workers; kwargs...)
 else
     addprocs(nb_workers; exeflags="--project=.")
