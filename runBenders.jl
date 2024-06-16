02dc404a7936ac2acec13120d9f7c8ef821f0234
Leonard Göke <lgoeke@ethz.ch>
2024-06-16 03:45:06 +0200

    corrected typo

--- runBenders.jl
+++ runBenders.jl
@@ -51,7 +51,7 @@ elseif accuracy == 3
 end
 
 # target gap, inaccurate cuts options, number of iteration after unused cut is deleted, valid inequalities, number of iterations report is written, time-limit for algorithm, distributed computing?, number of threads, optimizer
-algSetup_obj = algSetup(0.005, 20, (bal = false, st = false), 10, 4320.0, true, t_int, Gurobi.Optimizer, rngVio_ntup, conSub = (rng = [1e-2, 1e-8], int = inAcc_sym, crs = false))
+algSetup_obj = algSetup(0.005, 20, (bal = false, st = false), 10, 4320.0, true, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = inAcc_sym, crs = false))
 
 res_ntup = (general = (:summary, :exchange, :cost), carrierTs = (:electricity, :h2), storage = (write = true, agg = true), duals = (:enBal, :excRestr, :stBal))
 
