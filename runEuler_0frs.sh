9295cd87b2862d214deb5c63fb956395fe6a9df5
Leonard Göke <lgoeke@ethz.ch>
2024-06-28 10:13:14 +0200

    updated server scripts

--- runEuler_0frs.sh
+++ runEuler_0frs.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=report/benders_%j.out
 #SBATCH --error=report/benders_%j.err
 
-module add julia/1.10.2
-module add gurobi/10.0.1
+module add julia/1.10.3
+module add gurobi/10.0.3
 
 sbatch --nodes=3 --ntasks=3 --mem-per-cpu=8G --time=4380 --cpus-per-task=4 --ntasks-per-node=1 --wrap "julia --heap-size-hint=30G runBenders.jl $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK"
