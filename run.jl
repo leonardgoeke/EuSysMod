33b42e57ba054e957103806c3e9bd0e3e5f7adea
leonardgoeke <lgoeke@ethz.ch>
2024-07-14 20:02:25 +0200

    update

--- run.jl
+++ run.jl
@@ -1,6 +1,6 @@
 using AnyMOD, Gurobi, CSV, Statistics
 
-b = "C:/Git/EuSysMOD/"
+b = "C:/Users/pacop/Desktop/git/EuSysMOD/"
 
 par_df = CSV.read(b * "settings.csv", DataFrame)
 
@@ -16,7 +16,7 @@ space = string(par_df[id_int,:space]) # spatial resolution
 time = string(par_df[id_int,:time]) # temporal resolution
 scenario = string(par_df[id_int,:scenario]) # scenario case
 
-obj_str = space * "_" * time * "_testing"
+obj_str = space * "_" * time * "_" * scenario * "_testing"
 inputMod_arr = [b * "_basis", b * "timeSeries/" * space * "_" * time * "_" * scenario]
 resultDir_str = b * "results"
 
@@ -47,3 +47,39 @@ plotSankeyDiagram(anyM, dropDown = (:timestep,), ymlFilter = b * "all.yml")
 reportTimeSeries(:electricity, anyM)
 
 #endregion
+
+
+for x in keys(benders_obj.sub)
+    println(x)
+    optimize!(benders_obj.sub[x].optModel)
+    #compute_conflict!(benders_obj.sub[x].optModel)
+    println(value(sum(benders_obj.sub[x].parts.obj.var[:objVar][!,:var])))
+    printIIS(benders_obj.sub[x])
+end
+
+
+function printIIS(anyM::anyModel)
+
+    # computes iis
+    compute_conflict!(anyM.optModel)
+
+    if MOI.get(anyM.optModel, MOI.ConflictStatus()) != MOI.ConflictStatusCode(3) return end
+    # loops over constraint tables to find constraints within iis
+    allCns_pair = vcat(collect.(vcat(anyM.parts.obj.cns, anyM.parts.bal.cns, anyM.parts.cost.cns, anyM.parts.lim.cns, map(x -> x.cns, values(anyM.parts.exc))..., map(x -> x.cns, values(anyM.parts.tech))...))...)
+
+    for cns in allCns_pair
+        if cns[1] == :objEqn continue end
+
+        allConstr_arr = findall(map(x -> MOI.ConflictParticipationStatusCode(0) != MOI.get(anyM.optModel.moi_backend, MOI.ConstraintConflictStatus(), x.index), cns[2][!,:cns]))
+        # prints constraints within iis
+        if !isempty(allConstr_arr)
+            println("$(length(allConstr_arr)) of IIS in $(cns[1]) constraints.")
+            colSet_dic = Dict(x => Symbol(split(string(x), "_")[1]) for x in intCol(cns[2]))
+            for iisConstr in allConstr_arr
+                row = cns[2][iisConstr,:]
+                dimStr_arr = map(x -> row[x] == 0 ?  "" : x == :id ? string(row[x]) : string(x, ": ", join(getUniName(row[x], anyM.sets[colSet_dic[x]]), " < ")), collect(keys(colSet_dic)))
+                println("$(join(filter(x -> x != "", dimStr_arr), ", ")), constraint: $(row[:cns])")
+            end
+        end
+    end
+end
