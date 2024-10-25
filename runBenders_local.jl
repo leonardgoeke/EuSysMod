135668b9270b3baafe38d342716f7bed1d64ed8a
leonardgoeke <lgoeke@ethz.ch>
2024-10-25 14:59:09 +0200

    update local script

--- runBenders_local.jl
+++ runBenders_local.jl
@@ -44,13 +44,12 @@ else
 	numFoc_arr = [0,3]
 end
 
-
 if solve == "crsAllNoLim"
-	algSetup_obj = algSetup(0.005, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = true, meth = :barrier, timeLim = 0.0, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
+	algSetup_obj = algSetup(0.05, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = true, meth = :barrier, timeLim = 0.0, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
 elseif solve == "crsAll5Lim"
-	algSetup_obj = algSetup(0.005, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = true, meth = :barrier, timeLim = 0.01, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
+	algSetup_obj = algSetup(0.05, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = true, meth = :barrier, timeLim = 0.01, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
 elseif solve == "crsTop5Lim"
-	algSetup_obj = algSetup(0.005, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = false, meth = :barrier, timeLim = 5.0, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
+	algSetup_obj = algSetup(0.05, cutDel, (bal = false, st = true), 2, 4320.0, false, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = false, meth = :barrier, timeLim = 5.0, dbInf = true), (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true))
 end
 
 res_ntup = (general = (:summary, :exchange, :cost), carrierTs = (:electricity, :h2), storage = (write = true, agg = true), duals = (:enBal, :excRestr, :stBal))
@@ -101,20 +100,22 @@ scale_dic[:facSub] = (capa = 1e0, capaStSize = 1e2, insCapa = 1e0, dispConv = 1e
 
 #region # * prepare iteration
 
-
 # initialize distributed computing
 if algSetup_obj.dist
-	addprocs(SlurmManager(; launch_timeout = 300), exeflags="--heap-size-hint=60G", nodes=1, ntasks=1, ntasks_per_node=1, cpus_per_task=8, mem_per_cpu="8G", time=4380) # add all available nodes
-	rmprocs(wrkCnt + 2) # remove one node again for main process
+	addprocs(8)
+	#addprocs(SlurmManager(; launch_timeout = 300), exeflags="--heap-size-hint=60G", nodes=1, ntasks=1, ntasks_per_node=1, cpus_per_task=8, mem_per_cpu="8G", time=4380) # add all available nodes
+	#rmprocs(wrkCnt + 2) # remove one node again for main process
 	@everywhere begin
 		using Gurobi, AnyMOD
 		runSubDist(w_int::Int64, resData_obj::resData, rngVio_fl::Float64, sol_sym::Symbol, optTol_fl::Float64=1e-8, crsOver_boo::Bool=false, resultOpt_tup::NamedTuple=NamedTuple()) = Distributed.@spawnat w_int runSub(resData_obj, rngVio_fl, sol_sym, optTol_fl, crsOver_boo, resultOpt_tup)
 		getComVarDist(w_int::Int64) = Distributed.@spawnat w_int getComVar()
+		getSubStringDist(w_int::Int64, res_sym::Symbol) = Distributed.@spawnat w_int getSubString(res_sym)
 	end
 	passobj(1, workers(), [:info_ntup, :inputFolder_ntup, :scale_dic, :algSetup_obj])
 else
 	runSubDist = x -> nothing
 	getComVarDist = x -> nothing
+	getSubStringDist = x -> nothing
 end
 # create benders object
 benders_obj = bendersObj(info_ntup, inputFolder_ntup, scale_dic, algSetup_obj, stabSetup_obj, runSubDist, getComVarDist, res_ntup, nearOptSetup_obj);
@@ -122,63 +123,42 @@ benders_obj = bendersObj(info_ntup, inputFolder_ntup, scale_dic, algSetup_obj, s
 #endregion
 
 #region # * iteration algorithm
-while true
 
-	produceMessage(benders_obj.report.mod.options, benders_obj.report.mod.report, 1, " - Started iteration $(benders_obj.itr.cnt.i)", testErr = false, printErr = false)
+runIteration!(benders_obj, runSubDist)
 
-	#region # * solve top-problem and (start) sub-problems
-	str_time = now()
-	resData_obj, stabVar_obj = runTop(benders_obj);   
-	elpTop_time = now() - str_time
+#endregion
 
-	# start solving sub-problems
-	cutData_dic = Dict{Tuple{Int64,Int64},resData}()
-	timeSub_dic = Dict{Tuple{Int64,Int64},Millisecond}()
-	lss_dic = Dict{Tuple{Int64,Int64},Float64}()
-	numFoc_dic = Dict{Tuple{Int64,Int64},Int64}()
-
-	acc_fl = getConvTol(benders_obj.itr.gap, benders_obj.algOpt.gap, benders_obj.algOpt.sub.rng, benders_obj.algOpt.sub.int)
-
-	if benders_obj.algOpt.dist futData_dic = Dict{Tuple{Int64,Int64},Future}() end
-	for (id,s) in enumerate(sort(collect(keys(benders_obj.sub))))
-		if benders_obj.algOpt.dist # distributed case
-			futData_dic[s] = runSubDist(id + 1, copy(resData_obj), benders_obj.algOpt.rngVio.fix, benders_obj.algOpt.sub.meth, acc_fl, benders_obj.algOpt.sub.crs)
-		else # non-distributed case @suppress 
-			cutData_dic[s], timeSub_dic[s], lss_dic[s], numFoc_dic[s] =runSub(benders_obj.sub[s], copy(resData_obj), benders_obj.algOpt.rngVio.fix, benders_obj.algOpt.sub.meth, acc_fl, benders_obj.algOpt.sub.crs)
-		end
-	end
-
-	# top-problem without stabilization
-	if !isnothing(benders_obj.stab) runTopWithoutStab!(benders_obj, stabVar_obj) end
-
-	# get results of sub-problems
-	if benders_obj.algOpt.dist
-		wait.(collect(values(futData_dic)))
-		for s in sort(collect(keys(benders_obj.sub)))
-			cutData_dic[s], timeSub_dic[s], lss_dic[s], numFoc_dic[s] = fetch(futData_dic[s])
-		end
-	end
-	
-	#endregion
-
-	#region # * analyse results and update refinements
-
-	# update results and stabilization
-	updateIteration!(benders_obj, cutData_dic, resData_obj, stabVar_obj)
-	# report on iteration
-	reportBenders!(benders_obj, resData_obj, elpTop_time, timeSub_dic, lss_dic, numFoc_dic)
-
-	# check convergence and finish
-	rtn_boo = checkConvergence(benders_obj, lss_dic)
-	
-	#endregion
-	benders_obj.itr.cnt.i = benders_obj.itr.cnt.i + 1
-	if rtn_boo break end
-	
-end
 #region # * write results
 
 produceMessage(benders_obj.report.mod.options, benders_obj.report.mod.report, 1, " - Write results", testErr = false, printErr = false)
-writeBendersResults!(benders_obj, runSubDist, res_ntup)
+writeBendersResults!(benders_obj, runSubDist, getSubStringDist, res_ntup)
 
 #endregion
+
+#region # * compute dual variables for monte carlo analysis
+
+outDir_str = dir_str * "inputMonteCarlo/fixes/" * name_str * "/"
+
+writeVariableFix!(benders_obj, outDir_str)
+editTopForDuals!(benders_obj, inputFolder_ntup, info_ntup, stabSetup_obj, scale_dic, algSetup_obj, outDir_str, runSubDist)
+runIteration!(benders_obj, runSubDist)
+
+#endregion
+
+# TODO next: extract and write duals, how storage levels -> write montecarlo part 
+
+removeStab!(benders_obj)
+optimize!(benders_obj.top.optModel)
+
+
+
+for x in filter(x -> occursin("Benders",string(x)), keys(benders_obj.top.parts.lim.cns))
+	var_df = copy(benders_obj.top.parts.lim.cns[x])
+	var_df[!,:dual] = dual.(var_df[!,:cns])
+end
+
+var_df = copy(benders_obj.top.parts.tech[:gasStorage].cns[:expcStLvl])
+var_df[!,:dual] = dual.(var_df[!,:cns])
+select!(var_df,Not([:cns]))
+
+var_df[1,:cns]
