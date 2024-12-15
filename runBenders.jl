5f7a74c60b67c88985794dacb615bc629dcb971e
Leonard Göke <lgoeke@ethz.ch>
2024-12-15 16:13:48 +0100

    updated scripts

--- runBenders.jl
+++ runBenders.jl
@@ -55,7 +55,7 @@ else
 	meth_tup = tuple()
 end
 
-stabSetup_obj = stabSetup(meth_tup, 0.0, :reduced, 0.01, (upper = 50, inter = :log, sub = 10.0), true) # :none for last argument will skip initialization, other names just used for setting input folder below
+stabSetup_obj = stabSetup(meth_tup, 0.0, :reduced, 0.01, (upper = 1, inter = :lin, sub = 0.0), true) # :none for last argument will skip initialization, other names just used for setting input folder below
 
 # ! options for near optimal
 
