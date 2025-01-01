98fd97befd443789e4f6880ba5716e0cb440ead8
Leonard Göke <lgoeke@ethz.ch>
2025-01-01 23:09:33 +0100

    update

--- runBenders.jl
+++ runBenders.jl
@@ -45,7 +45,6 @@ rngTar_tup = (mat = (1e-2, 1e5), rhs = (1e-2, 1e2))
 # target gap, inaccurate cuts options, number of iteration after unused cut is deleted, valid inequalities, number of iterations report is written, time-limit for algorithm, distributed computing?, number of threads, optimizer, solver settings sub and top
 rngVio_ntup = (stab = 2e1, cut = 1e2, fix = 1e2)
 
-
 algSetup_obj = algSetup(0.01, cutDel, (bal = false, st = true), 2, 7200.0, true, t_int, Gurobi.Optimizer, rngVio_ntup, (rng = [1e-2, 1e-8], int = :none, crs = false, meth = :barrier, timeLim = 20.0, dbInf = true, check = false), (numFoc = [0,2,3], dnsThrs = dnsThrs, crs = false, qtrTol = 1e-6, feasTol = 1e-6))
 res_ntup = (general = (:summary, :exchange, :cost), carrierTs = (:electricity, :h2), storage = (write = true, agg = true), duals = (:enBal, :excRestr, :stBal))
 
