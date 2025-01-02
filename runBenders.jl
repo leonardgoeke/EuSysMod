a2aa601496c56bcb5d8f993582aeefd2d7bee804
Leonard Göke <lgoeke@ethz.ch>
2025-01-02 22:51:38 +0100

    removed worker removal from benders script

--- runBenders.jl
+++ runBenders.jl
@@ -122,7 +122,7 @@ end
 # initialize distributed computing
 if algSetup_obj.dist
 	addprocs(SlurmManager(; launch_timeout = 300), exeflags="--heap-size-hint=" * string(floor(t_int * ram) - 2 ) * "G", nodes=1, ntasks=1, ntasks_per_node=1, cpus_per_task=t_int, mem_per_cpu= string(ram) * "G", time=6000) # add all available nodes
-	rmprocs(wrkCnt + 2) # remove one node again for main process
+	# rmprocs(wrkCnt + 2) # remove one node again for main process
 	@everywhere begin
 		using Gurobi, AnyMOD
 		runSubDist(w_int::Int64, resData_obj::resData, rngVio_fl::Float64, sol_sym::Symbol, optTol_fl::Float64=1e-8, crsOver_boo::Bool=false, check_boo::Bool=false, resultOpt_tup::NamedTuple=NamedTuple()) = Distributed.@spawnat w_int runSub(resData_obj, rngVio_fl, sol_sym, optTol_fl, crsOver_boo, check_boo, resultOpt_tup)
