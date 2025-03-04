4bbb0dfcb33ec237ebfc2ecbf18e37a78cd501a2
leonardgoeke <lgoeke@ethz.ch>
2025-03-04 09:47:55 +0100

    updated setup

--- startMono_12threads_16ram.sh
+++ startMono_12threads_16ram.sh
@@ -8,7 +8,7 @@
 module add julia/1.10.3
 module add gurobi/12.0.1
 
-sbatch --nodes=1 --ntasks=1 --mem-per-cpu=16G --time=4320 --cpus-per-task=8 --ntasks-per-node=1 --wrap "julia --heap-size-hint=120G runMono.jl  $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK"
+sbatch --nodes=1 --ntasks=1 --mem-per-cpu=16G --time=4320 --cpus-per-task=12 --ntasks-per-node=1 --wrap "julia --heap-size-hint=188G runMono.jl  $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK"
 
 
 
