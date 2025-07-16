8e139b21139b03a700864a1355a53b9c3a94e4bb
leonardgoeke <lgoeke@ethz.ch>
2025-07-16 19:30:58 +0200

    bugfix

--- runBenders.jl
+++ runBenders.jl
@@ -8,7 +8,7 @@ setupDir_str = dir_str *  "modelSetup/"
 par_df = CSV.read(dir_str * "settings.csv", DataFrame)
 
 if isempty(ARGS)
-    id_int = 2
+    id_int = 25
     t_int = 4
 else
     id_int = parse(Int,ARGS[1])
@@ -24,18 +24,22 @@ security = string(par_df[id_int,:security]) # security settings
 inOos = string(par_df[id_int,:inputOutOfSample]) # capacity folder for out-of-sample testing
 
 # extract benders settings
-solve = par_df[id_int,:solve]
+
 wrkCnt = par_df[id_int,:workerCnt]
 cores_int = par_df[id_int,:cores]
 t_int = par_df[id_int,:threads]
 ram = par_df[id_int,:ram]
 cutDel = string(par_df[id_int,:cutDel])
 trust = par_df[id_int,:trust]
+
+weigthStab = par_df[id_int,:weigthStab]	
+optTolStab = par_df[id_int,:optTolStab]
+viStorage = par_df[id_int,:viStorage] == "TRUE"
+infeasTop = par_df[id_int,:infeasTop]
 dnsThrs = par_df[id_int,:dnsThrs]
 
 name_str = convert(String,par_df[id_int,:name])
 checkDet_boo = scenario in "scr" .* string.(1982:2016)
-
 
 # create files determining scenario setup
 scrQrt_arr, scrDir_str = generateScrInfo(checkDet_boo, scenario, setupDir_str)
@@ -44,8 +48,10 @@ scrQrtHeu_arr, scrDirHeu_str = generateScrInfo(false, "total12_ext0_" * split(sc
 #region # * options for algorithm
 
 # ! options for general algorithm
+
+# range violations
 rngTar_tup = (mat = (1e-2, 1e5), rhs = (1e-2, 1e2))
-rngVio_ntup = (stab = 2e2, cut = 1e1, fix = 1e1)
+rngVio_ntup = (stab = 2e2, cut = 1e2, fix = 1e1)
 
 # method for cut management
 if cutDel == "10cnt_1thres"
@@ -76,26 +82,31 @@ elseif cutDel == "100cnt_025thres"
 	del_int = 100
 	del_fl = 0.25
 end
-cutMgm_tup = (meth = :slack, opt = (cnt = del_int, thres = del_fl), freq = 1, report = false) 
 
-# range violations
-rngTar_tup = (mat = (1e-2, 1e5), rhs = (1e-2, 1e2))
-rngVio_ntup = (stab = 2e1, cut = 1e1, fix = 1e1)
+cutMgm_tup = (meth = :slack, opt = (cnt = del_int, thres = del_fl), freq = 1, report = false)
 
-# tolerance without stabilization
+# tolerance stabilized problem, convergence
+tolStab_arr = [optTolStab, optTolStab]
+interStab_sym = :log
+
+# tolerance stabilized problem, quadratic convergence
+tolStabQ_arr = [optTolStab, optTolStab]
+interStabQ_sym = :log
+
+# tolerance without stabilization, convergence
 tolNoStab_arr = [1e-2, 1e-6]
 interNoStab_sym = :lin
 
-# tolerance stabilized problem (normal, quadratic,and feas)
-tolStab_arr = [1e-2, 1e-6]
-interStab_sym = :log
+# tolerance for feasibility
+tolStabFeas_arr = [1e-4, 1e-6]
+interStabFeas_sym = :lin
 
 # solver options for sub and top problem
 subOpt_tup = (rng = [1e-2, 1e-8], int = :none, crs = false, meth = :barrier, timeLim = 30.0, dbInf = true, threads = t_int, check = false)
-topOpt_tup = (numFoc = numFoc_arr, dnsThrs = dnsThrs, crs = true, stabTol = (interStab_sym, tolStab_arr), stabTolQ = (interStab_sym, tolStab_arr), stabTolFeas = (interStab_sym, tolStab_arr), noStabTol =  (interNoStab_sym, tolNoStab_arr), stabMeth = 2, noStabMeth = 2, threads = t_int, check = false)
+topOpt_tup = (numFoc = [0,2,3], dnsThrs = dnsThrs, crs = false, stabTol = (interStab_sym, tolStab_arr), stabTolQ = (interStab_sym, tolStab_arr), stabTolFeas = (interStabFeas_sym, tolStabFeas_arr), noStabTol =  (interNoStab_sym, tolNoStab_arr), stabMeth = 2, noStabMeth = 2, threads = t_int, check = true)
 
 # target gap, inaccurate cuts options, number of iteration after unused cut is deleted, valid inequalities, number of iterations report is written, time-limit for algorithm, distributed computing?, number of threads, optimizer, solver settings sub and top
-algSetup_obj = algSetup(0.001, cutMgm_tup, (bal = false, st = true), 2, 7200.0, wrkCnt != 1, Gurobi.Optimizer, rngVio_ntup, subOpt_tup, topOpt_tup)
+algSetup_obj = algSetup(0.001, cutMgm_tup, (bal = false, st = viStorage), 2, 7200.0, wrkCnt != 1, Gurobi.Optimizer, rngVio_ntup, subOpt_tup, topOpt_tup)
 
 res_ntup = (general = (:summary, :exchange, :cost), carrierTs = (:electricity, :h2), storage = (write = true, agg = true), duals = (:enBal, :excRestr, :stBal))
 
@@ -111,9 +122,11 @@ end
 
 # solve frequency of top problem without stabilization for lower bound
 noStab_tup = (upper = 70, inter = :log, sub = 2.0)
-if solve == "onlyCapaStab"
+
+# weight of variables in stabilization
+if weigthStab == "noStLvl"
 	w_tup = (capa = 1e0, capaStSize = 1e0, stLvl = 0.0, lim = 1e0)
-elseif solve == "allStab"
+elseif weigthStab == "withStLvl"
 	w_tup = (capa = 1e0, capaStSize = 1e0, stLvl = 1e0, lim = 1e0)
 end
 
@@ -131,14 +144,14 @@ nearOptSetup_obj = nothing # cost threshold to keep solution, lls threshold to k
 # ! general problem settings
 
 # name, temporal resolution, level of foresight, superordinate dispatch level, length of steps between investment years
-info_ntup = (name = name_str, frsLvl = checkDet_boo ? 0 : 3, supTsLvl = 2, repTsLvl = 4, shortExp = 5) 
+info_ntup = (name = name_str, frsLvl = checkDet_boo ? 0 : 3, supTsLvl = 2, repTsLvl = 4, shortExp = 5, infeasTop = infeasTop != "none") 
 
 # ! input folders
-inDir_arr = [modDir_str * "basis", modDir_str * "infeasParameter", setupDir_str * "securitySetup/" * security, setupDir_str * "techSetup/" * techs, setupDir_str * "importCase/" * imp, setupDir_str * "resolution/" * reso, scrDir_str, modDir_str * "timeSeries/country_" * time * "_month/general"]
+inDir_arr = [modDir_str * "basis", modDir_str * "infeasParameter", setupDir_str * "securitySetup/" * security, setupDir_str * "infeasTop/" * infeasTop, setupDir_str * "techSetup/" * techs, setupDir_str * "importCase/" * imp, setupDir_str * "resolution/" * reso, scrDir_str, modDir_str * "timeSeries/country_" * time * "_month/general"]
 foreach(x -> push!(inDir_arr, modDir_str * "timeSeries/country" * "_" * time * "_month/general_" * x), unique(getindex.(scrQrt_arr,2)))
 foreach(x -> push!(inDir_arr, modDir_str * "timeSeries/country" * "_" * time * "_" * "month/" * x[1] * "/" * x[2]), scrQrt_arr)
 
-heuDir_arr = [modDir_str * "basis", modDir_str * "infeasParameter", setupDir_str * "securitySetup/" * security, setupDir_str * "techSetup/" * techs, setupDir_str * "importCase/" * imp, setupDir_str * "resolution/" * reso, scrDirHeu_str, modDir_str * "timeSeries/country_672h_month/general"]
+heuDir_arr = [modDir_str * "basis", modDir_str * "infeasParameter", setupDir_str * "securitySetup/" * security, setupDir_str * "infeasTop/" * infeasTop, setupDir_str * "techSetup/" * techs, setupDir_str * "importCase/" * imp, setupDir_str * "resolution/" * reso, scrDirHeu_str, modDir_str * "timeSeries/country_672h_month/general"]
 foreach(x -> push!(heuDir_arr, modDir_str * "timeSeries/country_672h_month/general_" * x), unique(getindex.(scrQrtHeu_arr,2)))
 foreach(x -> push!(heuDir_arr, modDir_str * "timeSeries/country_672h_month/" * x[1] * "/" * x[2]), scrQrtHeu_arr)
 
