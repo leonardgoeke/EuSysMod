ea0d9c387ff74448d40c8f1ce4aad36bd54555cb
leonardgoeke <lgoeke@ethz.ch>
2025-10-15 17:44:44 +0200

    bugfix

--- runBenders.jl
+++ runBenders.jl
@@ -22,7 +22,7 @@ foresight = par_df[id_int,:foresight] # scenario case
 optTolStab = par_df[id_int,:optTolStab]
 cutDel = string(par_df[id_int,:cutDel])
 
-lowLimStab = string(par_df[id_int,:lowLimStab]) |> (x -> x == "Inf" ? Inf : parse(Float64,x))
+lowLimStab = string(par_df[id_int,:lowLimStab]) |> (x -> x == "Inf" ? - Inf : parse(Float64,x))
 weigthStab = string(par_df[id_int,:weigthStab]) 
 viStorage = par_df[id_int,:viStorage] == "TRUE"
 infeasTop = par_df[id_int,:infeasTop]
@@ -113,7 +113,7 @@ elseif weigthStab == "withStLvl"
 end
 
 # method, threshold serious step, initialization, minimum value, solve frequency without stabilization, weights in stabilization (in additon to scaling of base problem)
-stabSetup_obj = stabSetup(meth_tup, 0.0, :reduced, - lowLimStab, (upper = 13, inter = :log, sub = 10.0), repVio = true, weight = w_tup)
+stabSetup_obj = stabSetup(meth_tup, 0.0, :reduced, lowLimStab, (upper = 13, inter = :log, sub = 10.0), repVio = true, weight = w_tup)
 
 # ! options for near optimal
 
